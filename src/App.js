e8594880e7863259104bf3a69accb6530c860991
Ha-g <ha-g@Ha-gs-MacBook-Pro.local>
2019-11-21 14:21:59 -0800

    quote showing up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
 import './App.css';
-
+require('dotenv').config();
 function App() {
+  const [quotes, setQuotes] = useState([])
+  const [pics, setPics] = useState([])
+
+
+  useEffect( () => {
+    axios.get('https://api.kanye.rest/')
+      .then(response => {
+        setQuotes(response.data)
+      })
+  },[])
+  
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      {quotes.quote}
+      {pics}
+
+    </>
+  )
 }
 
 export default App;
