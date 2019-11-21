cedadea7accfc9692ca427b30519c3504a09748b
kelcc <kelcc169@yahoo.com>
2019-11-21 15:34:45 -0800

    routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import './App.css';
-import React, { useEffect, useState } from 'react';
-import Unsplash from 'unsplash-js';
-import QuoteDisplay from './QuoteDisplay';
 import './css/HomePage.css';
-import dotenv from 'dotenv';
-dotenv.config()
-
-const unsplash = new Unsplash({ accessKey: process.env.ACCESS_KEY });
+import React from 'react';
+import QuoteDisplay from './QuoteDisplay';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => {
 
   return (
-    <div className="landing-background">
-      <header className="App-header">
-        <QuoteDisplay />
-        <p>
-          Answers, you seek....
-        </p>
-        <form>
-          <input type='text' placeholder='Find your Kan-Yoda'></input> <br />
-          <input type='submit'></input>
-        </form>
-      </header>
-    </div>
+    <Router>
+      {/* <Route exact path='/' render={<LandingPage /> } /> */}
+      <Route path='/quotes' render={<QuoteDisplay />} />
+    </Router>
   );
 }
 
--- src/QuoteDisplay.jsx
+++ src/QuoteDisplay.jsx
@@ -1,40 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import Unsplash from 'unsplash-js';
+import Unsplash, { toJson } from 'unsplash-js';
 import axios from 'axios';
 import dotenv from 'dotenv';
 dotenv.config()
 
-const unsplash = new Unsplash({ accessKey: process.env.ACCESS_KEY });
+const unsplash = new Unsplash({ accessKey: process.env.ACCESS_KEY, secret: process.env.SECRET_KEY });
 
 const QuoteDisplay = () => {
   const [ photo, setPhoto ] = useState();
   const [ yodaQuote, setYodaQuote ] = useState();
 
-  useEffect(() => {
-    axios.get('https://api.kanye.rest')
-      .then(response => {
-        console.log(response.data.quote)
-        axios.get(`http://yoda-api.appspot.com/api/v1/yodish?text=${response.data.quote}`)
-          .then(response => {
-            console.log(response.data.yodish)
-            setYodaQuote(response.data.yodish)
-          }).catch(error => {
-            console.log('yoda error')
-          })
-      }).catch(error => {
-        console.log('kanye error')
-      })
+  // useEffect(() => {
+  //   axios.get('https://api.kanye.rest')
+  //     .then(response => {
+  //       console.log(response.data.quote)
+  //       axios.get(`http://yoda-api.appspot.com/api/v1/yodish?text=${response.data.quote}`)
+  //         .then(response => {
+  //           console.log(response.data.yodish)
+  //           setYodaQuote(response.data.yodish)
+  //         }).catch(error => {
+  //           console.log('yoda error')
+  //         })
+  //     }).catch(error => {
+  //       console.log('kanye error')
+  //     })
 
   //   unsplash.photos.getRandomPhoto()
-  //   .then(response => {
-  //     return response.json()
-  //   }).then(jsonData => {
-  //     console.log(jsonData.urls.full)
-  //     setBackground(jsonData.urls.full)
-  //   }).catch(error => {
-  //     console.log('oops!')
-  //   })
-  },[])
+  //     .then(toJson)
+  //     .then(json => {
+  //       console.log(json.urls.full)
+  //     })
+  // },[])
 
   return(
     <div>
