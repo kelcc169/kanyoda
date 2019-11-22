ddf5076af9913b4b017e8374b11c66de76f9c862
kelcc <kelcc169@yahoo.com>
2019-11-21 17:54:37 -0800

    basic next and back functioning

--- src/QuoteDisplay.jsx
+++ src/QuoteDisplay.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import Unsplash, { toJson } from 'unsplash-js';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import dotenv from 'dotenv';
 import yoda from './img/Yoda_Cloud.png'
@@ -8,8 +9,13 @@ dotenv.config()
 const unsplash = new Unsplash({ accessKey: '', secret: process.env.SECRET_KEY });
 
 const QuoteDisplay = () => {
-  const [ photo, setPhoto ] = useState();
+  const [ photo, setPhoto ] = useState('');
   const [ yodaQuote, setYodaQuote ] = useState();
+  const [ next, setNext ] = useState(false);
+
+  function getNext() {
+    next ? setNext(false) : setNext(true)
+  }
 
   useEffect(() => {
     axios.get('https://api.kanye.rest')
@@ -25,18 +31,20 @@ const QuoteDisplay = () => {
         console.log('kanye error')
       })
 
-    unsplash.photos.getRandomPhoto({ query: 'what is the meaning of life' })
-      .then(toJson)
-      .then(json => {
-        setPhoto(json.urls.full)
-      })
-  },[])
+    // unsplash.photos.getRandomPhoto({ query: 'what is the meaning of life' })
+    //   .then(toJson)
+    //   .then(json => {
+    //     setPhoto(json.urls.full)
+    //   })
+  },[next])
 
   return(
     <div className='display' >
       <div className='quoteImage' style={{backgroundImage:`url(${photo})`}}></div>
       <img src={yoda} className='yoda' alt='yodas wisdom' />
       {yodaQuote ? <p className='quote' >{yodaQuote}</p> : <p></p> }
+      <button className='click'><Link to='/' >back</Link></button>
+      <button onClick={getNext} className='click'>next</button>
     </div>
   )
 }
